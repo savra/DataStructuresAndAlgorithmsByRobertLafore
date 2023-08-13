ac99cb970c98630996670c8cdc8847af9850225f
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-13 20:38:07 +0300

    Initial commit

---
+++ src/main/java/com/hvdbs/savra/datastructuresandalgorithmsbyrobertlafore/DataStructuresAndAlgorithmsByRobertLaforeApplication.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.datastructuresandalgorithmsbyrobertlafore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresAndAlgorithmsByRobertLaforeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAndAlgorithmsByRobertLaforeApplication.class, args);
+	}
+
+}
